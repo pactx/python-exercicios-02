9f3ae066cba6d43dee24dbf00ce7b1f3c539647a
Pact <seuemail@exemplo.com>
2025-09-19 23:53:06 -0300

    Adiciona exercício 001.py

---
+++ 001.py
@@ -0,0 +1,18 @@
+"""
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+
+
+
+try:
+    n = int(input('Digite um número: '))
+
+except ValueError:
+    print(f'O numero digitado não é um número inteiro.')
+
+else:
+    if n % 2 == 0:
+        print(f'O número {n} é um número inteiro PAR.')
+    else:
+        print(f'O número {n} é um número inteiro IMPAR.')
